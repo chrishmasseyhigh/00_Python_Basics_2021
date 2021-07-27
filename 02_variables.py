9a7a140025e8e996f4669976dab5c2a6cf922ae4
chrishmasseyhigh <87159815+chrishmasseyhigh@users.noreply.github.com>
2021-07-27 12:17:03 +1200

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,12 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 34
+
+# below is a constant (the name is UPERCASE)
+PI = 3.14
+
+num_2 = 2
